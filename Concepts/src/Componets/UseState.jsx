0cb2dca3c90708a08b00c7852d27387424db2415
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-04-03 07:44:54 +0530

    change in useState

--- Concepts/src/Componets/UseState.jsx
+++ Concepts/src/Componets/UseState.jsx
@@ -4,7 +4,7 @@ function UseState() {
   const [count, setCount] = useState(0);
 
   function incrementCount() {
-    setCount(count + 1);
+    setCount((prevCount) => prevCount + 1);
   }
   return (
     <>
